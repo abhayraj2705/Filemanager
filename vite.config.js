0f565408078100a8d1aec38b497cab88115573ad
abhayraj2705 <161270201+abhayraj2705@users.noreply.github.com>
2025-05-23 03:41:36 +0530

    up

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,7 +7,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  preview: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
   server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:5000',
